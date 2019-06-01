e554ca1ab282f323fa33840d3c11ce217b74999c
wsdfhjxc <wsdfhjxc@o2.pl>
2019-06-02 00:42:27 +0200

    Update dotfiles

---
+++ .mozilla/firefox/profile/user.js
@@ -0,0 +1,19 @@
+user_pref("gfx.webrender.all", true);
+user_pref("layers.acceleration.force-enabled", true);
+
+user_pref("general.autoScroll", true);
+user_pref("widget.content.gtk-theme-override", "Adwaita");
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+user_pref("network.cookie.cookieBehavior", 1);
+user_pref("browser.search.suggest.enabled", false);
+user_pref("browser.privatebrowsing.searchUI", false);
+user_pref("browser.startup.homepage", "about:blank");
+
+user_pref("font.default.x-western", "sans-serif");
+user_pref("font.name.monospace.x-western", "Roboto Mono");
+user_pref("font.name.sans-serif.x-western", "Roboto");
+user_pref("font.name.serif.x-western", "Roboto Slab");
+
+user_pref("xpinstall.signatures.required", false);
+user_pref("toolkit.telemetry.reportingpolicy.firstRun", false);
